1f04381b0fef8187746b119026e9fbabb2354839
514sid <140138716+514sid@users.noreply.github.com>
2024-05-06 22:53:08 +0400

    fix active tab border

--- src/components/PageTabs/PageTab.tsx
+++ src/components/PageTabs/PageTab.tsx
@@ -11,8 +11,8 @@ export const PageTab = ({
             to={ to }
             className={ ({ isActive }) =>
                 [
-                    "px-3 sm:px-7 h-10 sm:h-14 flex gap-3.5 items-center border-b-2 border-transparent transition-colors ease-in-out font-bold select-none text-sm sm:text-base",
-                    isActive ? "border-black text-black" : "text-neutral-400 hover:text-black"
+                    "px-3 sm:px-7 h-10 sm:h-14 flex gap-3.5 items-center border-b-2 transition-colors ease-in-out font-bold select-none text-sm sm:text-base",
+                    isActive ? "border-black text-black" : "text-neutral-400 hover:text-black border-transparent"
                 ].join(" ")
             }
             draggable={ false }
